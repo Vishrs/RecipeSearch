89ebb02994bfb18f787c96fd2c8bff62a0f94a9b
vishrs <utnip123@yahoo.com>
2020-01-12 14:28:30 -0500

    Add recipe component to map each recipe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
@@ -16,7 +17,7 @@ const App = () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
-
+    console.log(data.hits);
   }
   return (
     <div className="App">
@@ -24,6 +25,13 @@ const App = () => {
         <input className="search-bar" type="text"/>
         <button  className="search-button" type="submit"> Search </button>
       </form>
+
+      { recipes.map(
+          recipe => (
+            <Recipe/>
+          )
+        )
+      }
     </div>
   ) 
 }
